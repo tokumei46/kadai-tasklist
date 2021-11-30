5349c2b61ef4c311f3f80e9016d5f62efe89ddd5
tokumei <tana71058@gmail.com>
2021-11-30 11:27:37 +0000

    tasks

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,13 +1,10 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
       
-<<<<<<< HEAD
-        <a class="navbar-brand" href="/">tasklist</a>
-=======
-        <a class="navbar-brand" href="/">TaskList</a>
->>>>>>> 677a6bde75efba11d8ab9b107bfc0764d58e36cc
 
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+        <a class="navbar-brand" href="/">tasklist</a>
+        
+       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
 
