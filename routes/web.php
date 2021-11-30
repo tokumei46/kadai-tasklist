2204b2e1c44951e7c30f48457dcb10e0026d4f79
tokumei <tana71058@gmail.com>
2021-11-30 07:37:21 +0000

    task model

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+
+Route::get('tasks/{id}', 'TasksController@show');
+
+Route::post('tasks', 'TasksController@store');
+
+Route::put('tasks/{id}', 'TasksController@update');
+
+Route::delete('tasks/{id}', 'TasksController@destroy');
+
+
+Route::get('tasks', 'TasksController@index');
+
+Route::get('tasks/create', 'TasksController@create');
+
+
+
+Route::get('tasks', 'TasksController@index')->name('tasks.index');
+
+Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
+
+Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+
+
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
